b3b3921636264f419dab72ee4f508142a4132c12
junwei_zhou <1468235692@qq.com>
2022-09-27 07:49:41 +0800

    add:welcome page

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -2,15 +2,51 @@
 
 <template>
   <div class="welcome">
-    <h1>welcome</h1>
-  <router-link to="/login">跳转至登录页面</router-link>
+    <div class="content">
+      <div class="sub_title">欢迎体验</div>
+      <div class="title">通用后台管理系统</div>
+      <div class="desc">- Vue3.0+ElementPlus+Node+Mongo后台管理系统</div>
+    </div>
+    <div class="img">
+
+    </div>
   </div>
   </template>
 <script>
 
 </script>
-<style>
-  .welcome{
-
-  }
+<style scoped>
+.welcome {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  background-color: #fff;
+}
+.content {
+  position: relative;
+  bottom: 50px;
+}
+.sub-title {
+  font-size: 30px;
+  line-height: 42px;
+  color: #333;
+}
+.title {
+  font-size: 40px;
+  line-height: 62px;
+  color: #409eff;
+}
+.desc {
+  text-align: right;
+  font-size: 14px;
+  color: #999;
+  margin-left: 40px;
+}
+.img {
+  width: 371px;
+  height: 438px;
+  background-image: url('./../assets/images/welcome.png');
+  margin-left: 105px;
+}
 </style>
